3cfc1f018fac8683834b0e43976993283fdbe3c7
Shuangyi Chen <meetshuangyichen@outlook.com>
2022-10-01 12:07:19 -0400

    tcp test 3

--- rust/experiments/src/lib.rs
+++ rust/experiments/src/lib.rs
@@ -273,7 +273,7 @@ pub fn nn_server_c<R: RngCore + CryptoRng>(
         // let (mut reader_c_, mut writer_c_) = server_connect(server_c_addr);
         NNProtocol::offline_server_c_protocol_r2(
             &mut reader_c,
-            &mut reader_c,
+            &mut writer_c,
             rng,
             sc_state.num_relu,
             &mut sc_state,
