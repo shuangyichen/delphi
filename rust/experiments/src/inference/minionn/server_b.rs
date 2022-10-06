8ece684a0200b7706d8ef8d9634d8b591d978448
shuangyichen <meetshuangyichen.outlook.com>
2022-10-06 13:01:50 -0400

    test 16

--- rust/experiments/src/inference/minionn/server_b.rs
+++ rust/experiments/src/inference/minionn/server_b.rs
@@ -96,7 +96,7 @@ fn main(){
     // let port_c = args.value_of("port_a").unwrap_or("8000");
     // let server_c_addr = format!("{}:{}", ip_b, port_b);
     let server_a_addr = "10.30.8.15:8000";
-    let server_b_addr = "10.30.8.3:8000";
+    let server_b_addr = "10.30.8.11:8000";
     let server_c_addr = "10.30.8.7:8000";
 
 
