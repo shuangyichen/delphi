6d4270abe21a848ecbfbd824a234806a8c54d9e3
shuangyichen <meetshuangyichen.outlook.com>
2022-10-16 17:53:10 -0400

    comp 36

--- rust/protocols-sys/src/bindings.rs
+++ rust/protocols-sys/src/bindings.rs
@@ -646,8 +646,8 @@ fn bindgen_test_layout_ClientFHE() {
     );
 }
 #[repr(C)]
-#[derive(Debug)]
-pub struct ServerFHE {
+#[derive(Debug, Copy, Clone)]
+pub struct ServerFHe {
     pub context:    *mut ::std::os::raw::c_void,
     pub encoder:    *mut ::std::os::raw::c_void,
     pub encryptor:  *mut ::std::os::raw::c_void,
@@ -657,88 +657,89 @@ pub struct ServerFHE {
     pub zero:       *mut ::std::os::raw::c_char,
 }
 #[test]
-fn bindgen_test_layout_ServerFHE() {
+fn bindgen_test_layout_ServerFHe() {
     assert_eq!(
-        ::std::mem::size_of::<ServerFHE>(),
+        ::std::mem::size_of::<ServerFHe>(),
         56usize,
-        concat!("Size of: ", stringify!(ServerFHE))
+        concat!("Size of: ", stringify!(ServerFHe))
     );
     assert_eq!(
-        ::std::mem::align_of::<ServerFHE>(),
+        ::std::mem::align_of::<ServerFHe>(),
         8usize,
-        concat!("Alignment of ", stringify!(ServerFHE))
+        concat!("Alignment of ", stringify!(ServerFHe))
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).context as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).context as *const _ as usize },
         0usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(context)
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).encoder as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).encoder as *const _ as usize },
         8usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(encoder)
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).encryptor as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).encryptor as *const _ as usize },
         16usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(encryptor)
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).evaluator as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).evaluator as *const _ as usize },
         24usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(evaluator)
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).gal_keys as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).gal_keys as *const _ as usize },
         32usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(gal_keys)
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).relin_keys as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).relin_keys as *const _ as usize },
         40usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(relin_keys)
         )
     );
     assert_eq!(
-        unsafe { &(*(::std::ptr::null::<ServerFHE>())).zero as *const _ as usize },
+        unsafe { &(*(::std::ptr::null::<ServerFHe>())).zero as *const _ as usize },
         48usize,
         concat!(
             "Offset of field: ",
-            stringify!(ServerFHE),
+            stringify!(ServerFHe),
             "::",
             stringify!(zero)
         )
     );
 }
+pub type ServerFHE = ServerFHe;
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct RootServerMPHE {
