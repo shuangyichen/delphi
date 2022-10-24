769acc10868a4211e99f5f400c9276a5723e37ee
shuangyichen <meetshuangyichen.outlook.com>
2022-10-24 19:45:02 -0400

    resnet test 4

--- rust/protocols/src/neural_network.rs
+++ rust/protocols/src/neural_network.rs
@@ -1911,7 +1911,7 @@ where
         // println!("total layer {}",layer_total);
 
         for (i, layer) in neural_network_architecture.layers.iter().enumerate() {
-            if i< layer_total-2{
+            if i< layer_total-1{
             match layer {
                 LayerInfo::NLL(dims, NonLinearLayerInfo::ReLU) => {
                     relu_layers.push(i);
