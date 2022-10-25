387ec85682b05706dca3795fa110988f57a5fa4c
shuangyichen <meetshuangyichen.outlook.com>
2022-10-25 16:58:37 -0400

    experiments resnet32 split 30 1

--- rust/experiments/src/resnet32.rs
+++ rust/experiments/src/resnet32.rs
@@ -278,7 +278,7 @@ fn resnet_block_init<R: RngCore + CryptoRng>(
 ) {
     // conv_block_init(nn, vs, kernel_size, c_out, stride, relu_layers, rng,input_dims);
     iden_block_init(nn, vs, kernel_size, relu_layers, rng, input_dims);
-    for _ in 0..(layer_size - 2) {
+    for _ in 0..(layer_size - 3) {
         iden_block(nn, vs, kernel_size, relu_layers, rng)
     }
 }
@@ -310,7 +310,7 @@ fn resnet_1_block<R: RngCore + CryptoRng>(
     rng: &mut R,
 ) {
     conv_block(nn, vs, kernel_size, c_out, stride, relu_layers, rng);
-    // iden_block(nn, vs, kernel_size, relu_layers, rng);
+    iden_block(nn, vs, kernel_size, relu_layers, rng);
     // for _ in 0..layer_size-1 {
     //     iden_block(nn, vs, kernel_size, relu_layers, rng)
     // }
