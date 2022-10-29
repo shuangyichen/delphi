cc17683747f4337e875640fdcc623450d7ae9f6a
shuangyichen <meetshuangyichen.outlook.com>
2022-10-29 11:25:05 -0400

    46 1

--- rust/experiments/src/lib.rs
+++ rust/experiments/src/lib.rs
@@ -183,6 +183,10 @@ pub fn nn_user<R: RngCore + CryptoRng>(
         rng,
         &mut client_state,
     );
+    let reader_cost = reader_a.count();
+    let writer_cost = writer_a.count();
+    let ua_total_cost = reader_cost+writer_cost;
+    println!("U A preprocessing total cost {} bytes", ua_total_cost);
     // let duration1 = start_user.elapsed();
     NNProtocol::online_user_protocol(
         &mut reader_a, 
@@ -191,6 +195,9 @@ pub fn nn_user<R: RngCore + CryptoRng>(
         &architecture1,
         &client_state
     );
+    let reader_cost_ = reader_a.count();
+    let writer_cost_ = writer_a.count();
+    println!("U A online total cost {} bytes", reader_cost_-reader_cost+writer_cost_-writer_cost);
     // let duration1 = start_user.elapsed();
 
     println!("User Server finish eval");
@@ -234,13 +241,15 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
     // println!("server b connected");
     // let (mut reader_c, mut writer_c) = client_connect(server_c_addr);
     // println!("server c connected");
-    let start1 = Instant::now();
-    let start_user = Instant::now();
+    // let start1 = Instant::now();
+    let start_pre_ua = Instant::now();
     //***************Split 1 preprocessing  *********
     let (mut sa_split1,pk,sfhe) =  NNProtocol::offline_server_linear_protocol(&mut reader_u, &mut writer_u, &nn1, rng).unwrap();
-    let duration_user_1 = start_user.elapsed();
-    //***************Split 2 preprocessing   **********
+    let duration_pre_ua_1 = start_pre_ua.elapsed();
+    // println!("Preprocessing of M1: {:?}", duration_user_1);
 
+    //***************Split 2 preprocessing   **********
+    let start_pre_abc_1 = Instant::now();
     let (mut sa_state,cpk,rsmphe,lsmphe) = {
         let (mut sa_state,cpk ,rsmphe_,lsmphe_) = NNProtocol::offline_server_a_protocol(
                         &mut reader_b,
@@ -253,9 +262,9 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
                
                  (sa_state,cpk,rsmphe_,lsmphe_)
             };
+    let duration_pre_abc_1 = start_pre_abc_1.elapsed();
     //l+1 layer linear preprocessing
-    let start_user_2 = Instant::now();
-    let start_l_process = Instant::now();
+    let start_pre_abc_2 = Instant::now();
     NNProtocol::offline_server_a_l_protocol(
         &mut reader_u,
         &mut writer_u,
@@ -269,10 +278,10 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
         &lsmphe,
         &mut sa_state,
     );
-    let duration_l_process = start_l_process.elapsed();
-    println!("Preprocessing Time for l layer: {:?}", duration_l_process);
+    let duration_pre_abc_2 = start_pre_abc_2.elapsed();
+    
     println!("l layer preprocessed");
-
+    let start_pre_ua_2 = Instant::now();
     NNProtocol::offline_server_relu_protocol(
         &mut reader_u,
         &mut writer_u,
@@ -280,14 +289,16 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
         rng,
         &mut sa_split1,
     );
+    let duration_pre_ua_2 = start_pre_ua_2.elapsed();
+
     println!("User A relu done");
-    let duration_user_2 = start_user_2.elapsed();
-    let duration1 = start1.elapsed();
+    // let duration_user_2 = start_user_2.elapsed();
+    // let duration1 = start1.elapsed();
 
     // thread::sleep(time::Duration::from_millis(1000));
-    let (mut reader_a, mut writer_a) = server_connect(server_a_addr);
+    // let (mut reader_a, mut writer_a) = server_connect(server_a_addr);
     // let start = Instant::now();
-    let start2 = Instant::now();
+    let start_relu_abc = Instant::now();
     NNProtocol::offline_server_a_protocol_r2(
         &mut reader_b,
         &mut writer_b,
@@ -296,20 +307,22 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
         sa_state.num_relu,  //?
         &mut sa_state,
     );
-    let duration2 = start2.elapsed();
-    thread::sleep(time::Duration::from_millis(4000));
-    println!("Offline r2 done");
+    let duration_relu_abc = start_relu_abc.elapsed();
+    thread::sleep(time::Duration::from_millis(3000));
     let (mut reader_c, mut writer_c) = client_connect(server_c_addr);
 
-    let start3 = Instant::now();
+    let start_relu_abc_2 = Instant::now();
     NNProtocol::offline_server_a_protocol_r3(
         &mut reader_c,
         sa_state.num_relu,
         &mut sa_state,
     );
-    let duration3 = start3.elapsed();
-    let duration = duration3+duration2+duration1;
-    println!("Preprocessing Time from A: {:?}", duration);
+    let duration3_relu_abc_2 = start_relu_abc_2.elapsed();
+    // let duration = duration3+duration2+duration1;
+    let duration_pre_ua = duration_pre_ua_1+duration_pre_ua_2;
+    let duration_pre_abc = duration_pre_abc_1+duration_relu_abc+duration3_relu_abc_2;
+    println!("Preprocessing Time U-A part1: {:?}", duration_pre_ua);
+    println!("Preprocessing Time ABC : {:?}", duration_pre_abc);
 
     
 
@@ -319,9 +332,10 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
     //U-------A online
     // let (mut reader_a, mut writer_a) = server_connect(server_a_addr);
     // let start_a_online_1 = Instant::now();
-    let start_user_3 = Instant::now();
+    let start_user_a = Instant::now();
     let next_input = NNProtocol::online_root_server_protocol(&mut reader_u, &mut writer_u, &nn1, &sa_split1).unwrap();
-    let duration_user_3 = start_user_3.elapsed();
+    let duration_inf_ua = start_user_a.elapsed();
+    println!("Online Time ua : {:?}", duration_inf_ua);
     // //A----B----C online
     let last_share = NNProtocol::online_server_a_protocol(
         server_a_addr,
@@ -332,7 +346,7 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
         &sa_state,
     );
     // let duration = start_a_online.elapsed();
-    println!("Online Time of split 1 : {:?}", duration_user_3);
+    // println!("Online Time ua : {:?}", duration_inf_ua);
 
 
     //Output
@@ -343,7 +357,7 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
     // let (mut reader_u, mut writer_u) = client_connect(user_addr);
 
     // let out_channel = architecture2.layers[]
-    let start_user_4 = Instant::now();
+    // let start_user_4 = Instant::now();
     NNProtocol::root_server_output(
         &mut writer_u,
         &mut reader_b,
@@ -355,13 +369,13 @@ pub fn nn_root_server<R: RngCore + CryptoRng>(
         sfhe,
         out_channel,
     );
-    let duration_user_4 = start_user_4.elapsed();
-    let duration_user = duration_user_4+duration_user_3+duration_user_1;
-    println!("User Online time 1: {:?}", duration_user_1);
-    // println!("User Online time 2: {:?}", duration_user_2);
-    println!("User Online time 3: {:?}", duration_user_3);
-    println!("User Online time 4: {:?}", duration_user_4);
-    println!("User Online time: {:?}", duration_user);
+    // let duration_user_4 = start_user_4.elapsed();
+    // let duration_user = duration_user_4+duration_user_3+duration_user_1;
+    // println!("User Online time 1: {:?}", duration_user_1);
+    // // println!("User Online time 2: {:?}", duration_user_2);
+    // println!("User Online time 3: {:?}", duration_user_3);
+    // println!("User Online time 4: {:?}", duration_user_4);
+    // println!("User Online time: {:?}", duration_user);
 }
 
 pub fn nn_server_a<R: RngCore + CryptoRng>(
@@ -456,8 +470,8 @@ pub fn nn_server_b<R: RngCore + CryptoRng>(
         // let (mut reader_a, mut writer_a) = client_connect(server_a_addr);
         thread::sleep(time::Duration::from_millis(2000));
         let (mut reader_c, mut writer_c) = client_connect(server_c_addr);
-        thread::sleep(time::Duration::from_millis(20000));
-        let (mut reader_a, mut writer_a) = client_connect(server_a_addr);
+        // thread::sleep(time::Duration::from_millis(2000));
+        // let (mut reader_a, mut writer_a) = client_connect(server_a_addr);
         // let (mut reader_a, mut writer_a) = client_connect(server_c_addr);
         NNProtocol::offline_server_b_protocol_r2(
             &mut reader_b,
@@ -468,8 +482,19 @@ pub fn nn_server_b<R: RngCore + CryptoRng>(
             sb_state.num_relu,
             &mut sb_state,
         );
+
+        let reader_cost = reader_b.count();
+        let writer_cost = writer_b.count();
+        let ab_total_cost = reader_cost+writer_cost;
+        println!("A B preprocessing total cost {} bytes", ab_total_cost);
+        let reader_c_cost = reader_c.count();
+        let writer_c_cost = writer_c.count();
+        let bc_total_cost = reader_c_cost+writer_c_cost;
+        println!("B C preprocessing total cost {} bytes", bc_total_cost);
         sb_state
     };
+    
+
     println!("Offline finished");
     //online
     // let (mut reader_b, mut writer_b) = server_connect(server_b_addr);
@@ -510,6 +535,7 @@ pub fn nn_server_c<R: RngCore + CryptoRng>(
             &nn,
             rng,
         ).unwrap();
+
         //preprocessing l
         NNProtocol::offline_server_c_l_protocol(
             &mut reader_c,
@@ -519,6 +545,9 @@ pub fn nn_server_c<R: RngCore + CryptoRng>(
             rng,
             &mut sc_state,
         );
+        let mut reader_cost = reader_c.count();
+        let mut writer_cost = writer_c.count();
+        let mut total_cost = reader_cost+writer_cost;
         let (mut reader_c, mut writer_c) = server_connect(server_c_addr);
         NNProtocol::offline_server_c_protocol_r2(
             &mut reader_c,
@@ -533,6 +562,10 @@ pub fn nn_server_c<R: RngCore + CryptoRng>(
             &mut writer_c,
             &mut sc_state,
         );
+        reader_cost = reader_c.count();
+        writer_cost = writer_c.count();
+        total_cost = total_cost+reader_cost+writer_cost;
+        println!("A C preprocessing total cost {} bytes", total_cost);
         sc_state
     };
     println!("Offline finished");
@@ -560,7 +593,7 @@ pub fn nn_server_c<R: RngCore + CryptoRng>(
 pub fn generate_random_number<R: Rng>(rng: &mut R) -> (f64, TenBitExpFP) {
     let is_neg: bool = rng.gen();
     let mul = if is_neg { -1.0 } else { 1.0 };
-    let float: f64 = rng.gen_range(-1.0,1.0);
+    let float: f64 = rng.gen();
     let f = TenBitExpFP::truncate_float(float * mul);
     let n = TenBitExpFP::from(f);
     (f, n)
