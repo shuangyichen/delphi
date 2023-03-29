cd85ea7092a834885f567833661d2677b61b9352
shuangyichen <meetshuangyichen.outlook.com>
2023-03-28 22:25:09 -0400

    test3

--- rust/experiments/src/inference/inference_minionn.sh
+++ rust/experiments/src/inference/inference_minionn.sh
@@ -18,8 +18,8 @@
 INDEX=$1
 IP_U="10.30.8.7" 
 IP_A="10.30.8.15"
-IP_B="10.30.8.11"
-IP_C="10.30.8.5"
+IP_B="10.30.8.5"
+IP_C="10.30.8.11"
 SPLIT=2
 
 python3 ../../../../python/extract_keras_weights.py 0 --weights_path  ../../../../python/minionn/pretrained/relu/model --split_layer $SPLIT
